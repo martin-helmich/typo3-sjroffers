702729d1eccf8db1740d3bd333087d802fa25943
Martin Helmich <martin.helmich@hs-osnabrueck.de>
2014-06-11 11:05:28 +0200

    Add TYPO3 6.2 requirement.

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,6 +34,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-6.2.99',
+		),
+	),
 );
 
 ?>
