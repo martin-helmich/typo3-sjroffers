860634a0c2c7b770e8d920765025c88d64d0c646
Martin Helmich <martin.helmich@hs-osnabrueck.de>
2014-06-11 11:02:55 +0200

    Add minimalist build script for TER packaging.

---
+++ build.php
@@ -0,0 +1,26 @@
+<?php
+
+$_EXTKEY = 'sjr_offers';
+require_once('ext_emconf.php');
+
+$conf = $EM_CONF[$_EXTKEY];
+
+$validate = function() {
+	echo("Performing syntax check on all files... ");
+	exec("find . -name '*.php' | xargs -n1 php -l");
+	echo("Done\n");
+};
+
+$createPackage = function() use ($_EXTKEY, $conf) {
+	if (strpos($conf['version'], '-dev') !== FALSE) {
+        echo("Do not package dev versions!\n");
+        return 1;
+	}
+
+	$filename = sprintf("../%s-%s.zip", $_EXTKEY, $conf['version']);
+	exec("zip -r $filename .");
+	echo("Created $filename.\n");
+};
+
+$validate();
+$createPackage();
